929b6ae9fa4370c349dcf3679845a0c37e2144b8
rUper <Franco.Venezia@hotmail.com>
2020-08-29 10:50:08 -0300

    modificado el atributo justify-content en el elemento padre

---
+++ cs.cs
@@ -0,0 +1,14 @@
+
+public class Tutorial
+{
+   public static void Main()
+   {
+        string productName = "tv";
+        int productYear = 2012;
+        float productPrice = 279.99f;
+      Console.WriteLine("productName: " + productName);
+      Console.WriteLine("productYear: " + productYear);
+      Console.WriteLine("productPrice: " + productPrice);
+
+   }
+}
